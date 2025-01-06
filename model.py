b81af5db931911fe7f24efd8ca2066a096925028
iopenattheclose <prupro69@gmail.com>
2025-01-06 19:17:40 +0530

    feed forward

--- model.py
+++ model.py
@@ -81,3 +81,72 @@ class FeedForwadBlock(nn.Module):
         #input is tensor with batch,seq_len,d_model --> Linear1 -> batch,seq_len,d_ff -->Linear2 -> batch,seq_len,d_model
         return self.linear2(self.dropout(torch.relu((self.linear1(x)))))
 
+class MultiHeadAttention(nn.Module):
+
+    def __init__(self,d_model: int, heads :int, dropout:float)->None:
+        super().__init__()
+        self.d_model = d_model
+        self.heads = heads
+        self.dropout = nn.Dropout(dropout)
+        assert d_model % heads == 0 ,"d_model is not divisible by number of heads"
+
+        self.dk  = d_model // heads
+
+        #initializing wq,wk and wv matrices (fully connected layer)
+
+        self.wq = nn.Linear(d_model,d_model)
+        self.wk = nn.Linear(d_model,d_model)
+        self.wv = nn.Linear(d_model,d_model)
+
+        self.wo = nn.Linear(d_model,d_model)
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, q, k, v , mask):
+        #mask is required when we dont some words to interact with other words
+        # make that value ie attn score as -inf so after softmax it is ~0
+
+        #for all below matrices, dimension is batch,seq_len,d_model -> batch,seq_len,d_model
+        query = self.wq(q) 
+        key = self.wk(k)
+        value = self.wv(v)
+
+        #divide these matrices as per num of heads
+        #batch,seq_len,d_model -> batch,seq_len,h,dk -> batch,h,seq_len,dk
+        #This swaps the second and third dimensions (sequence length and number of heads).
+        query = query.view(query.shape[0], query.shape[1], self.heads, self.dk).transpose(1,2)
+        key = key.view(key.shape[0], key.shape[1], self.heads, self.dk).transpose(1,2)
+        value = value.view(value.shape[0], value.shape[1], self.heads, self.dk).transpose(1,2)
+
+        x, self.attention_scores = MultiHeadAttention.attention(query,key,value,self.dropout)
+
+        #batch,h,seq_len,dk -> batch,seq_len,h,dk --> batch,seq_len,d_model
+        #concat all the heads into a single matrix of size batch, seq_len, d_model
+        x = x.transpose(1,2).contiguous().view(x.shape[0],-1, self.heads * self.dk)
+
+        #batch, seq_len, d_model -->batch, seq_len, d_model
+        return self.wo(x)
+
+    @staticmethod
+    def attention(query, key, value, mask, dropout: nn.Dropout):
+        dk = query.shape[-1]
+
+        #atch,seq_len,h,dk -> batch,h,seq_len,seq_len
+        attention_scores = (query @ key.transpose(-2, -1))/math.sqrt(dk)
+        if mask is not None:
+            attention_scores.masked_fill_(mask == 0, -1e9)
+        attention_scores = attention_scores.softmax(dim=-1) #(batch, h, seq_len, seq_len)
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+
+        return (attention_scores @ value), attention_scores
+
+
+
+
+
+
+
+
+
+          
