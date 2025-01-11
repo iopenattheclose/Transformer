c0f5db05c826a2b74b5a46bf4285e5b39725810f
iopenattheclose <prupro69@gmail.com>
2025-01-11 21:47:16 +0530

    train.py

--- train.py
+++ train.py
@@ -14,8 +14,8 @@ from pathlib import Path
 
 # Huggingface datasets and tokenizers
 from datasets import load_dataset
-from tokenizers import Tokenizer
-from tokenizers.models import WordLevel
+from tokenizer import Tokenizer
+from tokenizer import WordLevel
 from tokenizers.trainers import WordLevelTrainer
 from tokenizers.pre_tokenizers import Whitespace
 
