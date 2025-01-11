c88cf0ee293659e698b7bb2257a6d06f3fc40a15
iopenattheclose <prupro69@gmail.com>
2025-01-11 21:45:40 +0530

    train.py

--- train.py
+++ train.py
@@ -1,2 +1,23 @@
 import torch
 import torch.nn as nn
+
+import datasets
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader, random_split
+from torch.optim.lr_scheduler import LambdaLR
+
+import warnings
+from tqdm import tqdm
+import os
+from pathlib import Path
+
+# Huggingface datasets and tokenizers
+from datasets import load_dataset
+from tokenizers import Tokenizer
+from tokenizers.models import WordLevel
+from tokenizers.trainers import WordLevelTrainer
+from tokenizers.pre_tokenizers import Whitespace
+
+import torchmetrics
+from torch.utils.tensorboard import SummaryWriter
