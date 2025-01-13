1e492a73d65405ef303905b9f0b6d925930c6268
iopenattheclose <prupro69@gmail.com>
2025-01-13 22:04:23 +0530

    dataset.py

--- dataset.py
+++ dataset.py
@@ -1,1 +1,10 @@
 import torch
+import torch.nn as nn
+from torch.utils.data import Dataset
+
+
+class BiLingualDataset(Dataset):
+
+    def __init__(self, ds, tokenzier_src, tokenzier_tgt, src_lan, tgt_lang, seq_len)->None:
+        super().__init__()
+        
