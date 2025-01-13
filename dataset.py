36943355ee0f0e6afdae0dbcd9d77ca0033dfe22
iopenattheclose <prupro69@gmail.com>
2025-01-13 22:06:32 +0530

    dataset.py

--- dataset.py
+++ dataset.py
@@ -7,4 +7,18 @@ class BiLingualDataset(Dataset):
 
     def __init__(self, ds, tokenzier_src, tokenzier_tgt, src_lan, tgt_lang, seq_len)->None:
         super().__init__()
+
+        self.ds = ds
+        self.tokenzier_src = tokenzier_src
+        self.tokenzier_tgt = tokenzier_tgt
+        self.src_lan = src_lan
+        self.tgt_lang = tgt_lang
+
+        self.sos_token = torch.Tensor([tokenzier_src.token_to_id(['[SOS]'])], dtype = torch.int64)
+        self.eos_token = torch.Tensor([tokenzier_src.token_to_id(['[EOS]'])], dtype = torch.int64)
+        self.pad_token = torch.Tensor([tokenzier_src.token_to_id(['[PAD]'])], dtype = torch.int64)
+
         
+
+
+
